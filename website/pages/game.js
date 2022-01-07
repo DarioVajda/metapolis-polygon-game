d4c6c340b3c4a51e7bc2615122867394372628c8
Zdenja70 <bogdanzdravkovic04@gmail.com>
2022-01-07 15:18:02 +0100

    pokrece se sa npm run dev, na localhost3000 je

--- website/pages/game.js
+++ website/pages/game.js
@@ -4,8 +4,7 @@ import Nav from '../components/nav/Nav'
 export default function game() {
     return (
         <div>
-            <Nav />
-            Our game will be here
+            put game here
         </div>
     )
 }

aa9559c18ee50edb95a8c3373de7d5e488640c76
Marennn <mmarkomile@gmail.com>
2022-03-03 22:16:16 +0100

    purely aesthetic building system wtih shit gui, added models, TO BE FIXED: building is 3x3, should be 2x2

--- game_render/main.js
+++ game_render/main.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three';
 import * as buildingModule from './external/building';
 import * as statsModule from './external/building_stats';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { MeshBasicMaterial } from 'three';
@@ -13,24 +13,25 @@ import { GUI } from 'dat.gui'
 const loader = new GLTFLoader();
 
 
+
 // RENDER/CAMERA INITIALISATION //
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // SETTING DEFAULT VALUES //
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 // renderer.setSize(window.innerWidth*0.8, window.innerHeight*0.8);
-camera.position.setZ(120);
+camera.position.setX(-120);
 // camera.position.setX(120);
 camera.position.setY(120);
 camera.lookAt(0,0,0);            //points camera at (0,0,0)
-const gridDimensions = 200;
-const gridSize = 20;
+const gridDimensions = 100;
+const gridSize = 10;
 const plotSize = gridDimensions/gridSize;
 
 //for mouse
@@ -42,9 +43,21 @@ var intersected;
 const datGui  = new GUI({ autoPlace: true });
 datGui.domElement.id = 'gui' ;
 var buildFolder = datGui.addFolder('Build');
-var guiType={type:0 };
+var guiType={type:0 };  
 buildFolder.add(guiType,'type',0,2,1);
 buildFolder.open();
+
+// LANDSCAPE MODEL
+loader.load( './models/valley_lanscape2.glb', function ( gltf ) {
+  gltf.scene.name='landscape';
+  gltf.scene.position.set(20,-23,-22);
+  gltf.scene.rotateY(Math.PI)
+  gltf.scene.scale.multiplyScalar(120);
+  scene.add( gltf.scene );
+}, undefined, function ( error ) {
+  console.error( error );
+} );
+
 
 // GRID //
 //  const gridHelper = new THREE.GridHelper(gridDimensions,gridSize);
@@ -54,7 +67,7 @@ buildFolder.open();
 for (let i = 0; i < gridSize; i++) {
   for (let j = 0; j < gridSize; j++) {
     const plane= new THREE.PlaneGeometry(plotSize-1,plotSize-1);
-    const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+    const material = new THREE.MeshBasicMaterial( {color: 0xcccccc,opacity:0.2,transparent:true, side: THREE.DoubleSide} );
     const gridSquare = new THREE.Mesh( plane, material );
     gridSquare.position.set(i*plotSize-gridSize/2*plotSize+plotSize/2,0,j*plotSize-gridSize/2*plotSize-plotSize/2);
     gridSquare.rotateX(Math.PI/2);
@@ -64,33 +77,6 @@ for (let i = 0; i < gridSize; i++) {
     scene.add(gridSquare);
   }
 }
-
-/*
-// ADDING HARDCODED BUILDINGS //
-buildingModule.buildingList.forEach(element => {
-
-  const elementType = element.type.type;
-  console.log(elementType);
-
-  // loader.load( './models/house_placeholder.glb', function ( gltf ) {
-  loader.load( './models/' + elementType + '_placeholder.glb', function ( gltf ) {
-    const model = gltf.scene.children[0];
-
-    ////MENANJE VELICINE DA BI POPUNILI ODGOVARAJUCA POLJA
-    const sizeX = Math.abs(element.start.x-element.end.x)+1;
-    const sizeY = Math.abs(element.start.y-element.end.y)+1;
-    const posX = (element.end.x-element.start.x);
-    const posY = (element.end.y-element.start.y);
-    model.scale.set(model.scale.x*0.5*sizeX,model.scale.y*0.5,model.scale.z*0.5*sizeY);
-    model.position.set(element.start.x*plotSize-gridDimensions/2+plotSize/2+plotSize*posX/2 , 0 , element.start.y*plotSize-gridDimensions/2+plotSize/2+plotSize*posY/2);
-    /////OVAJ DEO SA MENJANJEM VELICINE JE PRIVREMEN I TREBAO BI BITI ZAMENJEN SAMO VECIM MODELIMA...
-
-    scene.add( gltf.scene );
-  }, undefined, function ( error ) {
-    console.error( error );
-  } );
-});
-*/
 
 //*** */ BUILDING SYSTEM ***//
 //////////////////////////////
@@ -112,13 +98,13 @@ document.addEventListener("mousemove", event=>{
     if(intersects[0].object.parent.parent == null){
       if (intersected != intersects[0].object) {
         if (intersected) {
-          intersected.object.material.color.set(0xffff00);
+          intersected.object.material.color.set(0xcccccc);
         }
         intersected = intersects[0];
-        intersected.object.material.color.set(0xffffff);
+        intersected.object.material.color.set(0x777777);
       } else {
         if (intersected) {
-          intersected.object.material.color.set(0xffff00);
+          intersected.object.material.color.set(0xcccccc);
         }
         intersected = null;
       }
@@ -155,11 +141,13 @@ document.addEventListener(
       console.log("z: ",hit.object.parent.parent.gridy);
     }
     else{
-      console.log("x: ",hit.object.gridx);
-      console.log("y: ",(hit.object.position.y));
-      console.log("z: ",hit.object.gridy);
+      // console.log("x: ",hit.object.gridx);
+      // console.log("y: ",(hit.object.position.y));
+      // console.log("z: ",hit.object.gridy);
+      var objectdimensions = statsModule.buildingDimensions.get(Object.values(statsModule.buildingTypes)[guiType.type]);
       var start = new buildingModule.Coordinate(hit.object.gridx, hit.object.gridy);
-      var end = new buildingModule.Coordinate(hit.object.gridx, hit.object.gridy);
+      var end = new buildingModule.Coordinate(hit.object.gridx+objectdimensions[0][0]-1, hit.object.gridy+objectdimensions[0][1]-1);
+      console.log(start,end);
       var buildType = Object.values(statsModule.buildingTypes)[guiType.type];
       build(start,end,buildType);
     }
@@ -181,21 +169,22 @@ document.addEventListener(
   function build(start,end,type){
     buildingModule.addBuilding(new buildingModule.Building(start, end, type, 0));
     sceneAdd(start,end,type,0);
-    console.log("built: ",type);
+    // console.log("built: ",type);
   }
 
   function sceneAdd(start,end,elementType,level){
     loader.load( './models/' + elementType + '_placeholder.glb', function ( gltf ) {
       const model = gltf.scene.children[0];
-  
+      const wholescene = gltf.scene;
      
       const posX = (end.x+start.x)/2;
-      const posY = (end.y+end.y)/2;
+      // console.log(start.x,end.x)
+      const posY = (end.y+start.y)/2;
 
-      model.position.set(plotSize*posX-gridSize*plotSize/2+plotSize/2 , 0 , plotSize*posY-gridSize*plotSize/2-plotSize/2);
-      console.log(model.position);
+      wholescene.position.set(plotSize*posX-gridSize*plotSize/2+plotSize/2 , 0 , plotSize*posY-gridSize*plotSize/2-plotSize/2);
+      // console.log(model.position);
 
-      model.scale.multiplyScalar(0.5);
+      wholescene.scale.multiplyScalar(0.5/3);
 
       gltf.scene.name=elementType;
       gltf.scene.gridx=start.x;
@@ -205,7 +194,7 @@ document.addEventListener(
     }, undefined, function ( error ) {
       console.error( error );
     } );
-  }
+}
 
 // LIGHTS //
 const ambientLight = new THREE.AmbientLight(0xFFFFFF,0.8);

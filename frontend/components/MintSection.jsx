17782987f3ab7ca174ecc07749ca561bc5565368
Dario Vajda <vajdadario@gmail.com>
2022-03-14 21:33:39 +0100

    Napravljen deo sajta gde su stvari za mintovanje

--- frontend/components/MintSection.jsx
+++ frontend/components/MintSection.jsx
@@ -1,23 +1,70 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import style from './styles/mintSection.module.css'
 
-const MintSection = ({maticMint, wethMint, connectWallet}) => {
-  const [citiesLeft, setCitiesLeft] = useState(8342);
+const MintSection = ({maticMint, wethMint, numOfNFTs}) => {
+  const [citiesLeft, setCitiesLeft] = useState(10000);
   const [num, setNum] = useState(1);
+  const [token, setToken] = useState(true); // false-MATIC, true-WETH
+
+  const [price, setPrice] = useState(0);
+  
+  let mintPressed;
+
+  const delay = async (time) => {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+  const getPrice = async (tokenType) => {
+    let p;
+    await delay(100);
+    p = await maticMint(true);
+    console.log(p);
+    if(p) setPrice(p);
+  }
+
+  const mint = async () => {
+    if(mintPressed) return;
+    
+    if(token) {
+      mintPressed = true;
+      await maticMint();
+      mintPressed = false;
+    }
+    else {
+      mintPressed = true;
+      await wethMint();
+      mintPressed = false;
+    }
+  }
+
+  const getCitiesLeft = async () => {
+    let left = await numOfNFTs();
+    setCitiesLeft(10000-left);
+  }
+
+  useEffect(() => {
+    getPrice(true);
+    getCitiesLeft();
+  }, []);
 
   return (
     <div className={style.mintSection}>
       <div className={style.nftsLeft}>Cities left to mint: {citiesLeft}</div>
-      <div>
-        Matic
-        <input type="range" min="0" max="1" />
-        Weth
-      </div>
-      <div className={style.mintButton} onClick={maticMint}>Mint: {num}</div>
+      <div className={style.mintButton} onClick={() => mint()}>Mint: {num}</div>
       <div className={style.slider}>
-       <input type="range" value={num} min="0" max="10" onChange={(e) => { setNum((e.target.value>0)?e.target.value:1) } } />
+        <input type="range" value={num} min="0" max="10" onChange={(e) => { setNum((e.target.value>0)?e.target.value:1);  } } />
+      </div>
+      <div>
+        {token?Math.round(num*price*100)/100:Math.round(num*0.1*100)/100} {token?'MATIC':'WETH'}
+      </div>
+      <div className={style.tokenChoice}>
+        MATIC
+        <div className={`${style.switch} ${token?style.switchMatic:style.switchWeth}`} onClick={() => {setToken(!token); }} >
+          <div className={`${style.switchCircle} ${token?style.circleMatic:style.circleWeth}`} />
+        </div>
+        WETH
       </div>
       <div className={style.maxMint}>Max per wallet: 10</div>
       <div className={style.mintEnds}>Minting ends on May 18th at 2pm CET</div>
--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -33,7 +33,6 @@ export default function Home() {
   }
 
   //#region Functions for minting the NFTs
-
   const ethToMatic = async (ethPrice) => {
     var res = await (await fetch("https://api.binance.com/api/v3/aggTrades?symbol=MATICETH")).json();
     var price = 0;
@@ -44,17 +43,22 @@ export default function Home() {
     return ethPrice / price;
   }
   
-  const maticMint = async () => {
+  const maticMint = async (onlyPrice) => {
     if(!window.ethereum || cityContract === undefined) {
       return;
     }
     await connectWallet();
     
-    let ethPrice = 0.001;
+    let ethPrice = 0.001; // this should be loaded from somewhere
     
     let maticPrice = await ethToMatic(ethPrice);
-    console.log(maticPrice);
+    // console.log(maticPrice);
     let epsilon = 0.1;
+
+    if(onlyPrice) {
+      return (maticPrice + epsilon);
+    }
+
     let tx = await cityContract.maticMint(1, { value: ethers.utils.parseEther(`${maticPrice + epsilon}`) });
     let receipt = await tx.wait();
     console.log('Mint receipt:', receipt);
@@ -62,7 +66,7 @@ export default function Home() {
     // initCity();
   };
   
-  const wethMint = async () => {
+  const wethMint = async (onlyPrice) => {
     if(!window.ethereum || cityContract === undefined) {
       return;
     }
@@ -71,8 +75,11 @@ export default function Home() {
     let balance = await wethContract.balanceOf(account);
     console.log('balance1: ', balance);
 
+    let wethPrice = 0.1;
+    if(onlyPrice) return wethPrice; // this returns the price if the function is called like this: wethMint(true);
+
     let reciept
-    let tx = await wethContract.increaseAllowance(cityContract.address, ethers.utils.parseEther('0.1')); // there should be a check if the current allowance is enough
+    let tx = await wethContract.increaseAllowance(cityContract.address, ethers.utils.parseEther(`${wethPrice}`)); // there should be a check if the current allowance is enough
     try { reciept = await tx.wait(); }
     catch(e) { console.log('error', e); }
 
@@ -89,6 +96,11 @@ export default function Home() {
 
   //#endregion
 
+  const numOfNFTs = async () => {
+    let list = await (await fetch('http://localhost:8000/leaderboard')).json();
+    return list.length;
+  }
+
   const connectWallet = async () => {
     const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
     return account;
@@ -97,7 +109,7 @@ export default function Home() {
   const mintERC20 = async () => {
     const addr = await connectWallet();
     const tx = await wethContract.mint(addr, ethers.utils.parseEther('1'));
-    const reciept = await tx.wait();
+    const receipt = await tx.wait();
     console.log(receipt);
     var balance = await wethContract.balanceOf(addr);
     balance = balance.toString();
@@ -116,7 +128,7 @@ export default function Home() {
       <main>
         <div>
           <Nav />
-          <div id="mint"><MintSection maticMint={() => maticMint()} wethMint={() => wethMint()} /></div>
+          <div id="mint"><MintSection maticMint={maticMint} wethMint={wethMint} numOfNFTs={numOfNFTs} /></div>
           <DevOptions mintERC20={() => mintERC20()} />
         </div>
         

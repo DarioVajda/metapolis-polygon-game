1d725659343b34f9385ed49da29b6f22a83b922d
Marko <mmarkomile@gmail.com>
2022-04-02 14:15:44 +0200

    fixed render window size, experimented with hover. Very cool

--- frontend/pages/game.jsx
+++ frontend/pages/game.jsx
@@ -6,11 +6,25 @@ import GameRender from '../components/GameRender'
 import {OrbitControls, Stars} from "@react-three/drei"
 import Link from "next/link"
 
-function Box(){
+function Box(props){
+  const [hovered, setHover] = useState(false)
   return(
-    <mesh>
+    <mesh 
+      {...props}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+        
       <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink"/>
+      <meshLambertMaterial attach="material" color={hovered? 'hotpink' : 'orange'}/>
+    </mesh>
+  )
+}
+
+function Plane(){
+  return(
+    <mesh position={[0,0,0]} rotation={[-Math.PI/2,0,0]}>
+      <planeBufferGeometry attach="geometry" args={[100,100]}/>
+      <meshLambertMaterial attach="material" color="lime"/>
     </mesh>
   )
 }
@@ -19,23 +33,32 @@ function Box(){
 const gameplay = () => {
   return (
     <>
-        <GameRender/>
-        <Link href="/"><a>Home</a></Link>
-        <Canvas 
-        // pixelRatio={window.devicePixelRatio}
-        onCreated={({ gl, scene }) => {
-          gl.toneMapping = THREE.ACESFilmicToneMapping
-          gl.outputEncoding = THREE.sRGBEncoding
-          scene.background = new THREE.Color('#373740')
-        }}>
-          <OrbitControls/>
-          <Stars/>
-          <ambientLight intensity={0.5}/>
-          <spotLight position={[10,15,10]} angle={0.3}
+      <GameRender/>
+      <Link href="/"><a>Home</a></Link>
+      <div style={{position: "fixed",
+        height: '90%',
+        width: '90%',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)'}}>
           
-          />
-          <Box/>
-        </Canvas>;
+
+          <Canvas 
+          // pixelRatio={window.devicePixelRatio}
+          onCreated={({ gl, scene }) => {
+            gl.toneMapping = THREE.ACESFilmicToneMapping
+            gl.outputEncoding = THREE.sRGBEncoding
+            scene.background = new THREE.Color('#373740')
+          }}>
+
+            <OrbitControls/>
+            <Stars/>
+            <ambientLight intensity={0.5}/>
+            <spotLight position={[10,15,10]} angle={0.3}/>
+            <Box position={[0,1,0]}/>
+            <Plane/>
+          </Canvas>
+      </div>
     </>
   )
 }

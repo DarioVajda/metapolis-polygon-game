bf78749b9d72dc01b4e5f13085617525729c4c84
Dario Vajda <vajdadario@gmail.com>
2022-04-03 16:43:45 +0200

    Napravljena animacija za pojavljivanje odgovora u faqs delu

--- frontend/components/FAQuestion.jsx
+++ frontend/components/FAQuestion.jsx
@@ -8,12 +8,12 @@ const FAQuestion = ({q, a, i}) => {
 
   return (
     <div className={styles.item}>
-      <div className={styles.question}  onClick={() => setShow(!show)}>
+      <div className={styles.question} onClick={() => setShow(!show)}>
         {i+1}. {q}
         <span>{show?'-':'+'}</span>
       </div>
       <div className={`${styles.answer} ${show?styles.showAnswer:''}`}>
-        {a}
+        {a.repeat(i+1)}
       </div>
     </div>
   )

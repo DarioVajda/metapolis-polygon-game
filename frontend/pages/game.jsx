51bde3d725bbb5ec06a7d4ecc60edd01fdcb1f84
Marko <mmarkomile@gmail.com>
2022-04-06 01:06:55 +0200

    Dodao build,remove system (ZA SADA SAMO KUCA); POSTOJI BUG: ekran se ne updateuje na remove gradjevine

--- frontend/pages/game.jsx
+++ frontend/pages/game.jsx
@@ -1,49 +1,20 @@
 
 import React, {useState, useRef, Suspense} from 'react'
-import {Canvas} from "@react-three/fiber";
 import * as THREE from 'three';
-import GameRender from '../components/GameRender'
-import {OrbitControls, useHelper, useGLTF, Bounds} from "@react-three/drei"
+import {OrbitControls, Bounds} from "@react-three/drei"
 import Link from "next/link"
-import { SpotLightHelper } from 'three';
+
+import Buildings from '../components/game/Buildings';
 
 //----COMPONENTS----//
 // import {Model} from '../components/game/Valley_lanscape2_real'
-import Model from '../components/game/House1'
+import Lights from '../components/game/Lights'
+import WorldCanvas from '../components/game/WorldCanvas';
+import Landscape from '../components/game/Valley_lanscape2_real'
+import { generateUUID } from 'three/src/math/MathUtils';
+import Grid from '../components/game/Grid';
 
 //----CONSTANTS----//
-const models = '../../models/' //MODELS FOLDER
-//GRID CONSTANTS//
-const gridDimensions = 100;
-const gridSize = 10;
-const plotSize = gridDimensions/gridSize;
-const shadowMapSize = [2048,2048]
-
-//setting up the canvas
-function WorldCanvas(props){
-  return(
-    <Canvas
-    {...props}
-    shadows orthographic
-    onCreated={({ gl ,scene, camera }) => {
-      gl.outputEncoding = THREE.sRGBEncoding
-      scene.background = new THREE.Color('#373740')
-      camera.lookAt([0,0,0])
-      gl.shadowMap.type= THREE.PCFSoftShadowMap;
-    }}
-    camera={{fov:75, position:[10,7,11], zoom:30}}>
-    </Canvas>
-  )
-}
-
-function Lights(){
-  return(
-    <>
-      <ambientLight intensity={0.1} />
-      <spotLight castShadow color="white" intensity={2} position={[-50, 50, 40]} angle={0.5} shadow-mapSize={shadowMapSize} />
-    </>
-  )
-}
 
 //MAIN COMPONENT
 const gameplay = () => {
@@ -59,16 +30,17 @@ const gameplay = () => {
           <WorldCanvas>
             <OrbitControls/>
             <Lights/>
+            <Grid/>
             <Suspense fallback={null}>
               <Bounds fit clip observe margin={1}>
-                <Model/>
+                <Landscape scale={120} position={[-20,-23,2]}/>
+                <Buildings/>
               </Bounds>
             </Suspense>
           </WorldCanvas>
       </div>
-      <GameRender/>
-      <Link href="/"><a>Home</a></Link>
-      
+      {/* <GameRender/> */}
+      <Link href="/"><a>Home</a></Link>      
     </>
   )
 }

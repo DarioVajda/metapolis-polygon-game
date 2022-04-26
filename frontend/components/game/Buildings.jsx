f4721e8178005e3840503e182e3e3658cbf44052
Marko <mmarkomile@gmail.com>
2022-04-27 00:23:40 +0200

    some small changes to ui

--- frontend/components/game/Buildings.jsx
+++ frontend/components/game/Buildings.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import House from './House'
-import Factory from './Factory'
 import { useBuildingStore } from './BuildingStore.js'
 import {gridDimensions,gridSize,plotSize} from './GridData'
+import House from './models/House'
+import Factory from './models/Factory'
+import Building from './models/Building'
 
 
 export default function Buildings() {
@@ -23,6 +24,13 @@ export default function Buildings() {
       const posY=(building.start[1]+building.end[1])/2
         const position = [(plotSize*posX-gridSize*plotSize/2+plotSize/2),0,(plotSize*posY-gridSize*plotSize/2-plotSize/2)];
         return <Factory scale={1/6} key={building.uuid} position={position}/>
+    }
+    else if(Type==='Building')
+    {
+      const posX=(building.start[0]+building.end[0])/2
+      const posY=(building.start[1]+building.end[1])/2
+        const position = [(plotSize*posX-gridSize*plotSize/2+plotSize/2),0,(plotSize*posY-gridSize*plotSize/2-plotSize/2)];
+        return <Building scale={1/6} key={building.uuid} position={position}/>
     }
 
     //add more later
--- frontend/components/game/HoverObject.jsx
+++ frontend/components/game/HoverObject.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import House from './House'
-import Factory from './Factory'
+import React, { useEffect } from 'react'
+import { useSpring, animated } from '@react-spring/three'
 import { useBuildingStore } from './BuildingStore.js'
 import { buildingTypes } from './BuildingTypes.js';
 import { useGLTF } from '@react-three/drei'
 import {gridDimensions,gridSize,plotSize} from './GridData'
-
+import House from './models/House'
+import Factory from './models/Factory'
+import Building from './models/Building'
 
 export default function HoverObject() {
     const selectedBuilding=useBuildingStore(state=>state.selectedBuilding)
-    let selectedBuildingType = selectedBuilding?buildingTypes[selectedBuilding][0]:null //checks if selectedBuilding is null
-    const hoveredXY=useBuildingStore(state=>state.hoveredXY)
+    let selectedBuildingType = selectedBuilding?buildingTypes[selectedBuilding][0]:null //checks if selectedBuilding is null - no building selected
+    const hoveredXYCurrent=useBuildingStore(state=>state.hoveredXYCurrent)      ///KADA SE I JEDAN OD OVA DVA PROMENI ONDA SE RERENDERUJE COMPONENT
+    const hoveredXYPrevious=useBuildingStore(state=>state.hoveredXYPrevious)    ///KADA SE I JEDAN OD OVA DVA PROMENI ONDA SE RERENDERUJE COMPONENT
     const buildMode=useBuildingStore(state=>state.buildMode)
 
     if (selectedBuildingType && buildMode) {
-        const posX=(hoveredXY.x+hoveredXY.x+selectedBuildingType.width)/2
-        const posY=(hoveredXY.y+hoveredXY.y+selectedBuildingType.height)/2
-        const position = [(plotSize*posX-gridSize*plotSize/2),0,(plotSize*posY-gridSize*plotSize/2-plotSize)];
+        const posXPrevious=(hoveredXYPrevious.x+hoveredXYPrevious.x+selectedBuildingType.width)/2       ///RACUNA POZICIJU NA GRIDU (ovo je float zbog gradjevina siroke 3 square itd)
+        const posYPrevious=(hoveredXYPrevious.y+hoveredXYPrevious.y+selectedBuildingType.height)/2
+        const positionPrevious = [(plotSize*posXPrevious-gridSize*plotSize/2),0,(plotSize*posYPrevious-gridSize*plotSize/2-plotSize)];  ///RACUNA 3D poziciju na mapi
+        const posXCurrent=(hoveredXYCurrent.x+hoveredXYCurrent.x+selectedBuildingType.width)/2
+        const posYCurrent=(hoveredXYCurrent.y+hoveredXYCurrent.y+selectedBuildingType.height)/2
+        const positionCurrent = [(plotSize*posXCurrent-gridSize*plotSize/2),0,(plotSize*posYCurrent-gridSize*plotSize/2-plotSize)];
+
+
         if(selectedBuildingType.type=='House')
         {
-            return <House scale={1/6} key={'hoverObject'} position={position}/>
+            return <House scale={1/6} key={'hoverObject'} position={positionCurrent}/>
         }
         else if(selectedBuildingType.type=='Factory')
         {
-            return <Factory scale={1/6} key={'hoverObject'} position={position}/>
+            return <Factory scale={1/6} key={'hoverObject'} position={positionCurrent}/>
         }
         else if(selectedBuildingType.type=='Building')
         {
-            // return <House scale={1/6} key={'hoverObject'} position={position}/>
-            return <></>
+            return <Building scale={1/6} key={'hoverObject'} position={positionCurrent}/>
         }
         else
         {

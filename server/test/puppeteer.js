ed7b12cbbaebd5a1f6d8b081019deab473b5e50a
Dario Vajda <vajdadario@gmail.com>
2022-10-15 11:35:35 +0200

    najminimalnija promena

--- server/test/puppeteer.js
+++ server/test/puppeteer.js
@@ -4,13 +4,16 @@ const delay = async (time) => {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
-const saveImage = async (data) => {
-    // return;
+const saveImage = async (data, { resolution } = {}) => {
+    resolution = resolution || 800;
+    data = JSON.stringify(data);
+
+    console.log({resolution});
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.setViewport({
-        height: 400,
-        width: 400
+        height: resolution,
+        width: resolution
     });
 
     await page.goto(`http://localhost:3000/city/preview?data=${data}`);
@@ -19,8 +22,6 @@ const saveImage = async (data) => {
     // await page.type('input', 'Ovo sam ukucao koristeci node.js program');
 
     await delay(5000);
-    // const content = await page.content()
-    // console.log(content);
 
     const res = await page.screenshot({path: 'localhost.png'});
     console.log(res);
@@ -28,7 +29,10 @@ const saveImage = async (data) => {
     await browser.close();
 }
 
-dataStr = '{"buildings":[{"start":{"x":2,"y":8},"end":{"x":2,"y":8},"type":"house","level":0,"orientation":2,"id":0},{"start":{"x":8,"y":9},"end":{"x":8,"y":9},"type":"house","level":0,"orientation":2,"id":1},{"start":{"x":18,"y":16},"end":{"x":18,"y":16},"type":"house","level":0,"orientation":4,"id":2},{"start":{"x":0,"y":1},"end":{"x":1,"y":2},"type":"building","level":0,"orientation":1,"id":3},{"start":{"x":11,"y":1},"end":{"x":12,"y":4},"type":"factory","level":0,"orientation":1,"id":4},{"start":{"x":13,"y":11},"end":{"x":14,"y":12},"type":"office","level":0,"orientation":3,"id":5},{"start":{"x":10,"y":12},"end":{"x":10,"y":12},"type":"store","level":0,"orientation":2,"id":6},{"start":{"x":10,"y":17},"end":{"x":10,"y":17},"type":"store","level":0,"orientation":2,"id":7},{"start":{"x":0,"y":0},"end":{"x":0,"y":0},"type":"house","level":0,"orientation":3,"id":10},{"start":{"x":11,"y":14},"end":{"x":14,"y":17},"type":"park","level":0,"orientation":4,"id":9}],"specialBuildings":[{"start":{"x":17,"y":1},"end":{"x":17,"y":1},"type":"statue","orientation":4,"id":0},{"start":{"x":12,"y":6},"end":{"x":12,"y":7},"type":"fountain","orientation":3,"id":1}],"specialBuildingCash":[],"money":130000,"owner":"0x764cDA7eccc6a94C157742e369b3533D15d047c0","incomesReceived":0,"created":true,"initialized":true,"theme":0,"buildingId":{"type":"BigNumber","hex":"0x0b"},"specialBuildingId":{"type":"BigNumber","hex":"0x02"},"normal":16,"educated":14,"normalWorkers":70,"educatedWorkers":40,"achievementList":[{"key":"greenCity","count":0,"completed":false},{"key":"educatedCity","count":0,"completed":false},{"key":"skyCity","count":0,"completed":false},{"key":"highEducation","count":0,"completed":false},{"key":"check4","count":0,"completed":false}],"income":394256,"score":2889792}'
-saveImage(dataStr);
+const resolution = 1000;
+let dataStr = '{"buildings":[{"start":{"x":2,"y":8},"end":{"x":2,"y":8},"type":"house","level":0,"orientation":2,"id":0},{"start":{"x":8,"y":9},"end":{"x":8,"y":9},"type":"house","level":0,"orientation":2,"id":1},{"start":{"x":18,"y":16},"end":{"x":18,"y":16},"type":"house","level":0,"orientation":4,"id":2},{"start":{"x":0,"y":1},"end":{"x":1,"y":2},"type":"building","level":0,"orientation":1,"id":3},{"start":{"x":11,"y":1},"end":{"x":12,"y":4},"type":"factory","level":0,"orientation":1,"id":4},{"start":{"x":13,"y":11},"end":{"x":14,"y":12},"type":"office","level":0,"orientation":3,"id":5},{"start":{"x":10,"y":12},"end":{"x":10,"y":12},"type":"store","level":0,"orientation":2,"id":6},{"start":{"x":10,"y":17},"end":{"x":10,"y":17},"type":"store","level":0,"orientation":2,"id":7},{"start":{"x":0,"y":0},"end":{"x":0,"y":0},"type":"house","level":0,"orientation":3,"id":10},{"start":{"x":11,"y":14},"end":{"x":14,"y":17},"type":"park","level":0,"orientation":4,"id":9}],"specialBuildings":[{"start":{"x":17,"y":1},"end":{"x":17,"y":1},"type":"statue","orientation":4,"id":0},{"start":{"x":12,"y":6},"end":{"x":12,"y":7},"type":"fountain","orientation":3,"id":1}],"specialBuildingCash":[],"money":130000,"owner":"0x764cDA7eccc6a94C157742e369b3533D15d047c0","incomesReceived":0,"created":true,"initialized":true,"theme":0,"buildingId":{"type":"BigNumber","hex":"0x0b"},"specialBuildingId":{"type":"BigNumber","hex":"0x02"},"normal":16,"educated":14,"normalWorkers":70,"educatedWorkers":40,"achievementList":[{"key":"greenCity","count":0,"completed":false},{"key":"educatedCity","count":0,"completed":false},{"key":"skyCity","count":0,"completed":false},{"key":"highEducation","count":0,"completed":false},{"key":"check4","count":0,"completed":false}],"income":394256,"score":2889792}'
+let dataObj = JSON.parse(dataStr);
+console.log(dataObj);
+saveImage(dataObj, { resolution });
 
 exports.saveImage = saveImage;

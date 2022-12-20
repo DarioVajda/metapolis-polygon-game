a23d8df1535f0b9eea7ca35357fb50202b4637f4
Dario Vajda <vajdadario@gmail.com>
2022-12-20 16:59:14 +0100

    male promene u sajtu, nesto je pocelo da sere

--- frontend/components/Home/FAQuestion.jsx
+++ frontend/components/Home/FAQuestion.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import styles from '../styles/faqs.module.css';
 
+import ArrowIcon from '../universal/icons/ArrowIcon';
+
 const FAQuestion = ({q, a, i}) => {
 
   const [show, setShow ] = useState(false);
@@ -10,7 +12,8 @@ const FAQuestion = ({q, a, i}) => {
     <div className={styles.item}>
       <div className={styles.question} onClick={() => setShow(!show)}>
         {i+1}. {q}
-        <span>{show?'-':'+'}</span>
+        <span><ArrowIcon direction={show?0:2} verticalFlip /></span>
+        {/* <span>{show?'-':'+'}</span> */}
       </div>
       <div className={`${styles.answer} ${show?styles.showAnswer:''}`}>
         {a.repeat(i+1)}
